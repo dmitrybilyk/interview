bbf7f9bdf206938bd88b364db3b3c65ba1e7c919
dmytro <dmitry.b@scopicsoftware.com>
2025-07-05 21:36:33 +0300

    twoSum

--- src/main/java/com/conduct/interview/leetcode/Complement.java
+++ src/main/java/com/conduct/interview/leetcode/Complement.java
@@ -27,7 +27,38 @@ public class Complement {
         return "";
     }
 
+    public static String twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return nums[i] + ", " + map.get(complement);
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return "";
+    }
+
+    public static String onSorted(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            int result = nums[left] + nums[right];
+            if (result == target) {
+                return nums[left] + ", " + nums[right];
+            } else if (result < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return "";
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(findComplementMap(new int[]{3, 5, 2, 1}, 5));
+//        System.out.println(twoSum(new int[]{3, 5, 2, 1}, 5));
+        System.out.println(onSorted(new int[]{1, 2, 3, 5}, 5));
     }
 }
