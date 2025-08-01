da59d09ab9df703cfea579bd600b00d8c133ae48
dmytro <dmitry.b@scopicsoftware.com>
2025-08-01 15:31:42 +0300

    interview

--- src/main/java/com/conduct/interview/interview/CacheService.java
+++ src/main/java/com/conduct/interview/interview/CacheService.java
@@ -1,24 +1,24 @@
-package com.conduct.interview.interview;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-
-// What you see is a piece of code which you've got to code review.
-// Basically this is a caching layer on top of a costly / heavy-computation external service.
-// Please review it and then propose and implement required improvements.
-
-
-public class CacheService {
-
-    private final ConcurrentHashMap<List<String>, List<String>> cache = new ConcurrentHashMap<>();
-
-    public List<String> get(String[] key) {
-            List<String> actualKey = List.of(key);
-            return cache.computeIfAbsent(actualKey, externalService.get(key));
-
-    }
-}
+//package com.conduct.interview.interview;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.concurrent.ConcurrentHashMap;
+//import java.util.concurrent.locks.ReentrantReadWriteLock;
+//
+//// What you see is a piece of code which you've got to code review.
+//// Basically this is a caching layer on top of a costly / heavy-computation external service.
+//// Please review it and then propose and implement required improvements.
+//
+//
+//public class CacheService {
+//
+//    private final ConcurrentHashMap<List<String>, List<String>> cache = new ConcurrentHashMap<>();
+//
+//    public List<String> get(String[] key) {
+//            List<String> actualKey = List.of(key);
+//            return cache.computeIfAbsent(actualKey, externalService.get(key));
+//
+//    }
+//}
