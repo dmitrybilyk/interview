6689af834a686e35435ea7966046b3e3860848ef
dmytro <dmitry.b@scopicsoftware.com>
2025-08-12 16:58:06 +0300

    interview

---
+++ src/main/java/com/conduct/interview/_20_algorythms_and_data_structures/linked_lists/LRUCacheTest.java
@@ -0,0 +1,68 @@
+package com.conduct.interview._20_algorythms_and_data_structures.linked_lists;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LRUCacheTest {
+    private final int maxCacheSize;
+    private final Map<Integer, LinkedListNode> cacheMap;
+    private LinkedListNode head;
+    private LinkedListNode tail;
+
+
+    public LRUCacheTest(int maxCacheSize) {
+        this.maxCacheSize = maxCacheSize;
+        this.cacheMap = new HashMap<>();
+    }
+
+    public String getValue(int key) {
+        LinkedListNode linkedListNode = cacheMap.get(key);
+
+        if (linkedListNode == null) {
+            return null;
+        }
+
+        removeNodeFromCache(linkedListNode);
+        addNodeToFront(linkedListNode);
+        return linkedListNode.value;
+    }
+
+    public void setKeyValue(int key, String value) {
+        LinkedListNode node = new LinkedListNode(key, value);
+        if (cacheMap.size() > maxCacheSize) {
+            removeNodeFromCache(tail);
+        }
+        addNodeToFront(node);
+        cacheMap.put(key, node);
+    }
+
+    private void addNodeToFront(LinkedListNode node) {
+        if (head != null) {
+            head.prev = node;
+            node.next = head;
+        }
+        head = node;
+    }
+
+    private boolean removeNodeFromCache(LinkedListNode node) {
+        if (node == tail) {
+            tail.prev.next = null;
+        }
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        return true;
+    }
+
+
+    private class LinkedListNode {
+        private int key;
+        private String value;
+        private LinkedListNode next;
+        private LinkedListNode prev;
+
+        public LinkedListNode(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
--- src/main/java/com/conduct/interview/_20_algorythms_and_data_structures/linked_lists/LruCacheCheck.java
+++ src/main/java/com/conduct/interview/_20_algorythms_and_data_structures/linked_lists/LruCacheCheck.java
@@ -5,7 +5,15 @@ import java.util.Map;
 
 public class LruCacheCheck {
 
-    private Map<Integer, String> cache = new HashMap<>();
+    private Map<Integer, LinkedListNode> cache;
+    private final int maxSize;
+    private LinkedListNode head;
+    private LinkedListNode tail;
+
+    LruCacheCheck(int maxSize) {
+        this.maxSize = maxSize;
+        cache = new HashMap<>();
+    }
 
     private static class LinkedListNode {
         private int key;
@@ -19,12 +27,36 @@ public class LruCacheCheck {
         }
     }
 
-    public void addItem() {
+    public String getValue(int key) {
+        LinkedListNode linkedListNode = cache.get(key);
 
     }
 
-    public boolean removeItem() {
-        return false;
+    public void setKeyValue(int key, String value) {
+        if (cache.size() >= maxSize) {
+            removeTailNode();
+        }
+        LinkedListNode node = new LinkedListNode(key, value);
+        addNodeToFront(node);
+        cache.put(key, node);
+
+    }
+
+    private void addNodeToFront(LinkedListNode node) {
+        if (head == null) {
+            head = node;
+            tail = node;
+        } else {
+            head.prev = node;
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public boolean removeTailNode() {
+        tail.prev.next = null;
+        tail = tail.prev;
+        return true;
     }
 
 }
