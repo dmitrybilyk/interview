11d7f739de8865a27964e45e7b47e610ba68e4b2
dmytro <dmitry.b@scopicsoftware.com>
2025-08-17 21:51:42 +0300

    multithreading issues

--- src/main/kotlin/Lambdas.kt
+++ src/main/kotlin/Lambdas.kt
@@ -20,16 +20,60 @@ fun main() {
     println(sebsAgeFunction())
     // 26
 
+    val result = with (GoodPerson("Dmytro", 44)) {
+        name = "Dmytro2"
+        age = 35
+        true
+    }
+
+    val listStrings = listOf("dfd", "gfgf", "erer")
+    println(createStringFromList(listStrings))
+
+    val betterPerson = BetterPerson(1).apply {
+        name = "Better name"
+        age = 33
+        blond = true
+        println(" The name is $name")
+    }
+
+    val oneMoreBetterPerson = BetterPerson(2).also {
+        it.name = "one more better"
+        println(it.name)
+    }
+
+    println(betterPerson)
+
+    println(result)
+
+}
+
+
+
+
+fun createStringFromList(list: List<String>): String {
+    return buildString {
+        for (element in list) {
+            append(" ")
+            append(element)
+        }
+    }
 }
 
 fun multipy(a: Int): Int {
     return a * a
 }
 
-data class GoodPerson(val name: String, val age: Int) {
+data class GoodPerson(var name: String, var age: Int) {
+
     fun multiply(a: Int): Int {
         return a * a
     }
+}
+
+data class BetterPerson(val id: Int) {
+    var name: String? = null
+    var age: Int? = 0
+    var blond: Boolean? = false
 }
 
 //fun handleComputation(id: String) {
