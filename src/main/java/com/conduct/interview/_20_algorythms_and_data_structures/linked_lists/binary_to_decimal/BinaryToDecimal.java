4a4579d05226e0e48d933e3f0b3b2899cd3c8b82
DmBilyk <dmitry.bilyk@gmail.com>
2025-09-10 22:44:06 +0300

    changes

---
+++ src/main/java/com/conduct/interview/_20_algorythms_and_data_structures/linked_lists/binary_to_decimal/BinaryToDecimal.java
@@ -0,0 +1,30 @@
+package com.conduct.interview._20_algorythms_and_data_structures.linked_lists.binary_to_decimal;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+public class BinaryToDecimal {
+    public static int getDecimalValue(ListNode head) {
+        int result = 0;
+        ListNode current = head;
+
+        while (current != null) {
+            // multiply by 2 (shift left) and add current bit
+            result = result * 2 + current.val;
+            current = current.next;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Example: binary 101
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(0);
+        head.next.next = new ListNode(1);
+
+        System.out.println(getDecimalValue(head)); // Output: 5
+    }
+}
