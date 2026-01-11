ebd785acf8fba80e8e1cc5fdae8f1c94ffec6879
dmytro <dmitry.bilyk@gmail.com>
2026-01-11 11:17:52 +0200

    kotlin

--- src/main/kotlin/knowyourproject/kotlin/dsl/DslCheck2.kt
+++ src/main/kotlin/knowyourproject/kotlin/dsl/DslCheck2.kt
@@ -1,42 +1,62 @@
-//package knowyourproject.kotlin.dsl
-//
-////Building>flat>Room
-//class DslCheck2 {
-//
-//}
-//
-//class Room {
-//    val name: String
-//    val area: Int
-//
-//    constructor(name: String, area: Int) {
-//        this.name = name
-//        this.area = area
-//    }
-//}
-//
-//class Flat {
-//    val number: Int
-//
-//    val rooms: List<Room>
-//
-//    constructor(rooms: List<Room>, number: Int) {
-//        this.rooms = rooms
-//        this.number = number
-//    }
-//}
-//
-//class Building {
-//    val number: Int
-//    val flats: List<Flat>
-//
-//    constructor(flats: List<Flat>, number: Int) {
-//        this.flats = flats
-//        this.number = number
-//    }
-//
-//}
-//
-//fun building(number: Int, block: Building.() -> Unit): Building {
-//    return Building(number).block()
-//}
+package knowyourproject.kotlin.dsl
+
+fun main() {
+    val building = building {
+        number = 1
+        flat {
+            number = 1
+            room {
+                name = "Bedroom"
+                area = 20
+            }
+            room {
+                name = "Bathroom"
+                area = 10
+            }
+        }
+    }
+
+    building.show()
+}
+
+@BuildingDsl
+class Room {
+    var name: String? = null
+    var area: Int? = 0
+}
+
+@BuildingDsl
+class Flat {
+    var number: Int? = null
+    var rooms: List<Room> = mutableListOf()
+
+    fun room(block: Room.() -> Unit): Room {
+        return Room().apply(block)
+    }
+}
+
+@BuildingDsl
+class Building {
+    var number: Int? = null
+    var flats: List<Flat> = mutableListOf()
+
+    fun flat(block: Flat.() -> Unit): Flat {
+        return Flat().apply(block)
+    }
+
+    fun show() {
+        flats.forEach { flat ->
+            println("Flat number: ${flat.number}")
+            flat.rooms.forEach { room ->
+                println("Room name: ${room.name}, area: ${room.area}")
+            }
+        }
+    }
+}
+
+fun building(block: Building.() -> Unit): Building {
+    return Building().apply(block)
+}
+
+@DslMarker
+annotation class BuildingDsl()
